40649ef08c41c618e91a019e866577a6ebb2ef96
Pavel Bucka <pavelbucka@icloud.com>
2015-02-10 15:31:51 +0100

    Add campaigns and links factories

---
+++ spec/factories/campaigns.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :campaign do
+    id 1
+    name 'DSL.SK'
+    url 'www.dsl.sk'
+    commission 20
+  end
+end
---
+++ spec/factories/links.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :link do
+    campaign
+    id 1
+    url 'www.dsl.sk/12345'
+  end
+end
