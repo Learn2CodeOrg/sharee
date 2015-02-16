d0f6b2df41f3172489f88fa1663cb4831f3a8bb4
Pavel Bucka <pavelbucka@icloud.com>
2015-02-16 14:53:30 +0100

    Define get_role_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource)
+    get_role_path
+  end
+
+  protected
+
+  def get_role_path
     current_user.merchant? ? campaigns_path : links_path
   end
 end
