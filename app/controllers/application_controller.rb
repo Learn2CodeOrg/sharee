cb9d014deb3be515f97e0cfc477c7361f90c014b
Pavel Bucka <pavelbucka@icloud.com>
2015-02-16 18:48:03 +0100

    Add :iban, :swift, :paypal to User edit form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,19 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :configure_devise_params, if: :devise_controller?
+
+  protected
+
+  def configure_devise_params
+    devise_parameter_sanitizer.for(:account_update) do |u|
+      u.permit(:email, :iban, :swift, :paypal, :password, :password_confirmation, :current_password)
+    end
+  end
+
   def after_sign_in_path_for(resource)
     get_role_path
   end
-
-  protected
 
   def get_role_path
     current_user.merchant? ? campaigns_path : links_path
