999016bca5229f94e9f684f1bf7bd77ebdc8850b
Pavel Bucka <pavelbucka@icloud.com>
2015-02-17 14:45:59 +0100

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150216165629) do
+ActiveRecord::Schema.define(version: 20150217133357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,8 +45,10 @@ ActiveRecord::Schema.define(version: 20150216165629) do
     t.string   "code"
     t.string   "email"
     t.decimal  "price",      precision: 8, scale: 2
-    t.datetime "created_at",                         null: false
-    t.datetime "updated_at",                         null: false
+    t.datetime "created_at",                                         null: false
+    t.datetime "updated_at",                                         null: false
+    t.boolean  "paid",                               default: false
+    t.decimal  "commission", precision: 8, scale: 2
   end
 
   create_table "users", force: :cascade do |t|
