c5b2808dd97193b4aafdc546d53e97a470f21eff
Pavel Bucka <pavelbucka@icloud.com>
2015-02-19 17:28:48 +0100

    Change smtp_settings variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,11 +79,11 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :authentication => :plain,
-    :address => "smtp.mailgun.org",
-    :port => 587,
-    :domain => "sharee.io",
-    :user_name => ENV["MAILGUN_USER_NAME"],
-    :password => ENV["MAILGUN_PASSWORD"]
+    authentication: :plain,
+    address:   ENV['SMTP_ADDRESS'],
+    port:      ENV['SMTP_PORT'],
+    domain:    ENV['SMTP_DOMAIN'],
+    user_name: ENV['SMTP_USER_NAME'],
+    password:  ENV['SMTP_PASSWORD']
   }
 end
