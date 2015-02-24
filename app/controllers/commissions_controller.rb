2eca5ab1d897ddcc7e97fd2ed18ff9abdafd61c5
Pavel Bucka <pavelbucka@icloud.com>
2015-02-24 17:46:27 +0100

    Allow user to select payment method
    * update CommissionsController
    * update commissions view

--- app/controllers/commissions_controller.rb
+++ app/controllers/commissions_controller.rb
@@ -5,25 +5,39 @@ class CommissionsController < ApplicationController
   respond_to :html, :js
 
   def index
-    user = current_user
-    links = Link.where(user: user)
+    @user = current_user
+    @payoff_day = 8
+    @is_payoff_day = Time.now.day >= @payoff_day
 
+    links = Link.where(user: @user)
     sell_action_ids = SellAction.where(link_id: links, claimed_at: nil, paid_at: nil).where('approved_at IS NOT NULL').pluck(:id)
-    session[:sell_action_ids] = sell_action_ids
 
     @commission = SellAction.where(id: sell_action_ids).sum('price * (commission/100)')
+    @commission_bottom_limit = 50
+    @is_commission_bottom_limit_met = @commission > @commission_bottom_limit
+
+    session[:sell_action_ids] = sell_action_ids
     session[:commission] = @commission
   end
 
   def pay
-    sell_action_ids = session[:sell_action_ids]
-    SellAction.where(id: sell_action_ids).update_all({claimed_at: Time.now})
-    session.delete(:sell_action_ids)
-
-    commission = session[:commission]
     user = current_user
+    commission = session[:commission]
+    sell_action_ids = session[:sell_action_ids]
+    payment_method = params[:payment_method]
+
+    SellAction.where(id: sell_action_ids).update_all(
+      {
+        claimed_at: Time.now,
+        iban: user.iban_referer,
+        swift: user.swift_referer,
+        paypal: user.paypal_referer,
+        payment_method: payment_method,
+      })
     UserMailer.claim_email(user, commission.to_s).deliver_later
     ShareeMailer.claim_email(user, commission.to_s).deliver_later
+
+    session.delete(:sell_action_ids)
     session.delete(:commission)
   end
 end
