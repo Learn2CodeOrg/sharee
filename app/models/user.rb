c1f9f405c147876e12107b6d9b34411508bc8bce
Pavel Bucka <pavelbucka@icloud.com>
2015-03-08 18:46:09 +0100

    Add :async to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :async
 
   has_many :campaigns
   has_many :links
