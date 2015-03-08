4c30a84403187e0516b374a25f8c2a1dedbac73f
Pavel Bucka <pavelbucka@icloud.com>
2015-03-08 15:05:35 +0100

    Add validations to Campaign model

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -3,4 +3,8 @@ class Campaign < ActiveRecord::Base
   has_many :links
   has_many :open_actions, through: :links
   has_many :sell_actions, through: :links
+  
+  validates :name, presence: true
+  validates :url, presence: true
+  validates :commission, presence: true, numericality: { greater_than: 0, less_than: 100 }
 end
