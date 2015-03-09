ba2375a432169165118cdeb83e7110cad3fa94fd
Pavel Bucka <pavelbucka@icloud.com>
2015-03-09 15:27:17 +0100

    Fix styles and scripts for sharee social buttons

--- public/sharee/scripts.js
+++ public/sharee/scripts.js
@@ -1,23 +1,23 @@
 $(function() {
   $('.sharee-fb').on('click', function(e) {
     e.preventDefault();
-    var url = 'https://www.facebook.com/sharer/sharer.php?u=' + getShareeUrl(sender);
+    var url = 'https://www.facebook.com/sharer/sharer.php?u=' + getShareeUrl(this);
     window.open(url,'_blank','width=800,height=300');
   });
 
   $('.sharee-tw').on('click', function(e) {
     e.preventDefault();
-    var url = 'http://twitter.com/share?url=' + getShareeUrl(sender) + '&text=Sharing';
+    var url = 'http://twitter.com/share?url=' + getShareeUrl(this) + '&text=Sharing';
     window.open(url,'_blank','width=800,height=300');
   });
 
   $('.sharee-gp').on('click', function(e) {
     e.preventDefault();
-    var url = 'https://plus.google.com/share?url=' + getShareeUrl(sender);
+    var url = 'https://plus.google.com/share?url=' + getShareeUrl(this);
     window.open(url,'','menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=400,width=500');
   });
 
-  var getShareeUrl = function(sener) {
+  var getShareeUrl = function(sender) {
     return $(sender).closest('.sharee-button-success-block').find('.sharee-link').val();
   };
 });
