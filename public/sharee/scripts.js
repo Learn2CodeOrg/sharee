ff0f537ee98a68f4fff2893db145c2ff60d35486
Pavel Bucka <pavelbucka@icloud.com>
2015-03-09 16:19:24 +0100

    Delegate calls for sharee's social buttons

--- public/sharee/scripts.js
+++ public/sharee/scripts.js
@@ -1,17 +1,17 @@
 $(function() {
-  $('.sharee-fb').on('click', function(e) {
+  $(document).on('click', '.sharee-fb', function(e) {
     e.preventDefault();
     var url = 'https://www.facebook.com/sharer/sharer.php?u=' + getShareeUrl(this);
     window.open(url,'_blank','width=800,height=300');
   });
 
-  $('.sharee-tw').on('click', function(e) {
+  $(document).on('click', '.sharee-tw', function(e) {
     e.preventDefault();
     var url = 'http://twitter.com/share?url=' + getShareeUrl(this) + '&text=Sharing';
     window.open(url,'_blank','width=800,height=300');
   });
 
-  $('.sharee-gp').on('click', function(e) {
+  $(document).on('click', '.sharee-gp', function(e) {
     e.preventDefault();
     var url = 'https://plus.google.com/share?url=' + getShareeUrl(this);
     window.open(url,'','menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=400,width=500');
