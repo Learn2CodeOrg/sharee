c43aaaef0ec9cefa7736d876e5213aa54a9e6910
Pavel Bucka <pavelbucka@icloud.com>
2015-03-11 13:36:53 +0100

    Remove :async from User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :invitable, :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :async
+  devise :invitable,
+    :database_authenticatable,
+    :registerable,
+    :recoverable,
+    :rememberable,
+    :trackable,
+    :validatable
 
   has_many :campaigns
   has_many :links
