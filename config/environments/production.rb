bedbd5ddfb714a202caa10cf1f109c9bcaef13d1
Pavel Bucka <pavelbucka@icloud.com>
2015-03-12 16:56:35 +0100

    Fix logo displaying in mailer.html.erb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'sharee.io' }
+  config.action_mailer.asset_host = 'http://www.sharee.io'
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     authentication: :plain,
