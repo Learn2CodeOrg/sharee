c07111ca1ca8652a19e78c6536945bcb0ce0fc4f
Pavel Bucka <pavelbucka@icloud.com>
2015-03-13 10:08:41 +0100

    Add IBAN to User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150312120810) do
+ActiveRecord::Schema.define(version: 20150313090023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -99,6 +99,7 @@ ActiveRecord::Schema.define(version: 20150312120810) do
     t.integer  "invited_by_id"
     t.string   "invited_by_type"
     t.integer  "invitations_count",      default: 0
+    t.string   "iban"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
