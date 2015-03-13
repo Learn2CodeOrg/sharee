f4182ec8a954a93b323688989ee405941524a781
Pavel Bucka <pavelbucka@icloud.com>
2015-03-13 12:50:03 +0100

    Change default from and reply_to

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -2,7 +2,7 @@ class DeviseMailer < Devise::Mailer
   include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
   default template_path: 'devise/mailer' # to make sure that your mailer uses the devise views
 
-  default from: 'no-reply@sharee.io'
-  default reply_to: 'no-reply@sharee.io'
+  default from: 'info@sharee.io'
+  default reply_to: 'info@sharee.io'
   layout 'mailer'
 end
